83f3ddd7fded546e06e6b2c0dfef45bb71fd925f
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-09-02 05:49:34 +0100

    Added author

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,29 @@
+/*
+BLUR-WALLPAPER.
+Project website: https://github.com/ppizarror/blur-wallpaper
+
+MIT License
+Copyright (c) 2017 Pablo Pizarro @ppizarror.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 // Console messages
 var consolemsglist = [];
 var maxmsg = 5;
@@ -19,6 +45,15 @@ function consolemsg(msg) {
         }
         $('#console #consoletext').html(msgc);
     } catch (e) {} finally {}
+}
+
+function setConsoleStatus(status) {
+    // Enable/Disable console
+    if (status) {
+        $('#console').css('display', 'block');
+    } else {
+        $('#console').css('display', 'none');
+    }
 }
 
 function clearRandomFunTimer() {
